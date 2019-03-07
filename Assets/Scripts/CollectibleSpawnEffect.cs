9d68a828120becb6e9788fa5bd4298e01f3e4dad
Nathaniel Jew <nathankjew@gmail.com>
2019-03-06 19:57:30 -0800

    Fixed project

--- Assets/Scripts/CollectibleSpawnEffect.cs
+++ Assets/Scripts/CollectibleSpawnEffect.cs
@@ -14,12 +14,13 @@ public class CollectibleSpawnEffect : MonoBehaviour {
 
     IEnumerator LifeSequence() {
         Vector3 startSize = transform.localScale;
+        maxSize = Vector2.Distance(transform.position, Camera.main.transform.position) * 3f;
         Vector3 endSize = new Vector3(maxSize, maxSize, 1f);
 
         float p = 0f;
         while (p < 1f) {
             transform.localScale = Vector3.Lerp(startSize, endSize, p);
-            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, Mathf.Lerp(1, 0, p));
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, Mathf.Lerp(0.5f, 0, p));
             yield return new WaitForEndOfFrame();
             p += Time.deltaTime / lifeTime;
         }
